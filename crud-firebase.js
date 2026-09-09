c1e31da9cc61d39aa9e4dde0c7814b30c6d187a3
Nicolas <n.diaz115@alumnos.santotomas.cl>
2026-09-08 21:52:01 -0300

    Crud Firebase

---
+++ crud-firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBg1FTlCFCnJWMmhGxh8CiXVBmGHEXNy1U",
+  authDomain: "inventarionube-76f44.firebaseapp.com",
+  projectId: "inventarionube-76f44",
+  storageBucket: "inventarionube-76f44.firebasestorage.app",
+  messagingSenderId: "617039842232",
+  appId: "1:617039842232:web:97c9c763c493a0798ddd4d"
+};
